5e67ca65279adbe9754aad43875c68f33c635da5
jaboston <jaboston89@gmail.com>
2019-06-03 15:57:24 +0200

    fixed a bug with the spacing when you contract the screen to less than 500px. Added a way to populate the pokemons in the list via javascript objects. Make it so that when you click on the items, the console will output the item you clicked on.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+// globally accessible element.
+// reference our list item for cloning.
+var $listItem = document.querySelector('.project-grid__item');
+
+
 var pokemonRepository = (function () {
   var repository = [];
 
@@ -26,26 +31,64 @@ var pokemonRepository = (function () {
 }
 )();
 
+function showDetails(pokemon){
+  //TODO: show pokemon details when requested
+  console.log("name: " + pokemon.name + ", height: " + pokemon.height + ", type: " + pokemon.type + "");
+}
+
+function addListItem(pokemon){
+  // check the values are legit
+  if(typeof(pokemon.height)==typeof(0)
+  &&
+  typeof(pokemon.name)==typeof("")
+  &&
+  typeof(pokemon.type)==typeof([])
+  ){
+
+  // clone our list item.
+  var $pokemonTemplatedItem = $listItem.cloneNode(false);
+  var $pokemonTemplatedButton = document.querySelector(".button--pokemon").cloneNode(false);
+
+  // find our project grid.
+  var $projectGrid = document.querySelector(".project-grid");
+
+  // set our row based on the pokemon properties. We already checked that pokemon name is a type of string in this
+  $pokemonTemplatedButton.innerText = pokemon.name;
+  $pokemonTemplatedItem.insertBefore($pokemonTemplatedButton, $pokemonTemplatedItem.firstElementChild);
+
+  $pokemonTemplatedButton.addEventListener('click', function () {
+    // we need to referenence a pokemon outside of this function just above.
+    showDetails(pokemon);
+  });
+
+  // insert our pokemon at the top.
+  $projectGrid.insertBefore($pokemonTemplatedItem, $projectGrid.firstElementChild);
+
+  }
+}
+
   var pikachu, bulbasaur, charmander;
   pikachu = {
-    name: "pikachu",
+    name: "Pikachu",
     height: 100,
     type: ["electric"]
   }
 
   bulbasaur = {
-    name: "bulbasaur",
+    name: "Bulbasaur",
     height: 80,
     type: ["grass"]
   }
 
   charmander = {
-    name: "charmander",
+    name: "Charmander",
     height: 120,
     type: ["fire"]
   }
 
-// push our defined pokemons to the repository through our add function.
+
+
+  // push our defined pokemons to the repository through our add function.
   pokemonRepository.add(pikachu);
   pokemonRepository.add(charmander);
   pokemonRepository.add(bulbasaur);
@@ -54,22 +97,14 @@ var pokemonRepository = (function () {
 
   //iterate through the repository looking for pokemons.
   for(var i = 0; i < pokemonRepository.getAll().length; i++){
-    // document.write("<h3>- " + pokemonRepository.getAll()[i].name + "</h3>");
+    addListItem(pokemonRepository.getAll()[i]);
     //using foreach
     Object.keys(pokemonRepository.getAll()[i]).forEach(function(property) {
       // dont repeat name
-      if(property != "name"){
-        // document.write("<div class='stats'>" + property + ": " + pokemonRepository.getAll()[i][property] + "</div>");
-      }
-      // document.write("<br/>");
+      // todo: if name already exists, remove it.
     });
-
-    // check if the height is that of a big boi
-    if(pokemonRepository.getAll()[i].height > 100){
-      // document.write("wow! " + pokemonRepository.getAll()[i].height + "cm... Thats a big boi!")
-      // document.write("</br>")
-    } else {
-      // document.write("just a little boi. it should stay in its pokeball out of shame.")
-      // document.write("</br>")
-    }
   }
+
+  // once we know we have added all the available pokemons, it is safe to delete our original $pokemonTemplatedItem
+  // delete our original list item because aint nobody got time for that.
+  $listItem.parentElement.removeChild($listItem);
